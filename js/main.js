7dbe1a502c0d7137c4f87ee43067452ab4f5566f
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-02 16:53:41 +0200

    [UPD]

--- js/main.js
+++ js/main.js
@@ -5,10 +5,29 @@ console.log('sono collegato');
 const generateButton = document.getElementById('generatore');
 //funzione del bottone generatore
 generateButton.addEventListener('click', function() {
+
+    //IMPUT
+    const nameInput = document.getElementById('name-user');
     const kmInput = document.getElementById('km-user').value;
     const ageInput = document.getElementById('eta-user');
     console.log('ho clicato il bottone genera');
     console.log({kmInput});
+
+    //OUTPUT
+    
+    const userOutput = document.getElementById('user-output');
+    const offertaOutput = document.getElementById('offerta-output');
+    const carrozzaOutput = document.getElementById('carrozza-output');
+    const codiceOutput = document.getElementById('codice-output');
+    const costoOutput = document.getElementById('costo-output');
+
+    console.dir(nameInput)
+    console.dir(userOutput)
+    console.dir(offertaOutput)
+    console.dir(carrozzaOutput)
+    console.dir(codiceOutput)
+    console.dir(costoOutput)    
+
     //dati calcolo biglietto
     const kmPrezzo = 0.21;
     const scontoUnder = 20;
@@ -30,4 +49,30 @@ generateButton.addEventListener('click', function() {
     //mostra anteprima biglietto
     bigliettoElement.classList.remove('nascosta')
 
+    //OUTPUT -BIGLIETTO GENERATO
+
+    userOutput.innerText = nameInput.value;
+    offertaOutput.innerText = ageInput.value;
+    
+    codiceOutput
+    costoOutput.innerText = prezzo.toFixed(2);
+
 });
+
+//variaboile bottone di reset 
+const resetButton = document.getElementById('reset');
+//funzione del bottone reset
+resetButton.addEventListener('click', function() {
+    //documenti html
+    const bigliettoElement = document.getElementById('biglietto');
+    //nascondi anteprima biglietto
+    bigliettoElement.classList.add('nascosta');
+    //svuotare i campi
+    const nameInput = document.getElementById('name-user');
+    const kmInput = document.getElementById('km-user');
+    const ageInput = document.getElementById('eta-user');
+    nameInput.value ='';
+    kmInput.value ='';
+    ageInput.value ='under';
+
+})
